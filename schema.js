6796c1962d3dc8e9ef97973e2686ba57ffdee34d
skeshavaa <s.keshavaa@gmail.com>
2020-05-20 20:21:16 -0400

    created project schema

--- schema.js
+++ schema.js
@@ -7,6 +7,28 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
+
+
+const ProjectType = new GraphQLObjectType({
+    name: 'Project',
+    fields: () => ({
+        id: {type:GraphQLString},
+        title: {type: GraphQLString},
+        desc: {type: GraphQLString},
+        image: {type: GraphQLList},
+        stack: {type: GraphQLList},
+        code: {type: GraphQLString},
+        demo: {type: GraphQLString}
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    project: {
+        type: ProjectType
+    }
+})
+
 module.exports = new GraphQLSchema({
 
 });
