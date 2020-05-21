441d6c6c8b9c310edfa73ae19f7442d9644563e5
skeshavaa <s.keshavaa@gmail.com>
2020-05-21 00:08:30 -0400

    adding query by tech stack soon

--- schema.js
+++ schema.js
@@ -8,12 +8,6 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
-// const projects = [
-//     {id: "1", title: "project1", desc: "desc for project1", image: ["fake image url"], stack: ['react', 'gatsby'],  code: "fake code url", project: "fake demo url"},
-//     {id: "2", title: "project2", desc: "desc for project2", image: ["fake image url"], stack: ['flutter', 'express'],  code: "fake code url", project: "fake demo url"},
-//     {id: "3", title: "project3", desc: "desc for project3", image: ["fake image url"], stack: ['mongodb', 'firebase'],  code: "fake code url", project: "fake demo url"}
-// ];
-
 const ProjectType = new GraphQLObjectType({
     name: 'Project',
     fields: () => ({
@@ -30,26 +24,35 @@ const ProjectType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
+        //Query for a specific project by ID
         project: {
             type: ProjectType,
             args:{
                 id:{type: GraphQLString}
             },
             resolve(parentValue, args){
-                // for (let i = 0; i < projects.length; i++){
-                //     if (projects[i].id == args.id){
-                //         return projects[i];
-                //     }
-                // }
                 return axios.get('http://localhost:3000/projects/' + args.id)
                     .then(res => res.data);
 
             }
         },
+        //Query for all projects
         projects: {
             type: new GraphQLList(ProjectType),
             resolve(parentValue, args){
                 return axios.get('http://localhost:3000/projects')
+                    .then(res => res.data)
+            }
+        },
+
+        //Query project by Tech Stack
+        projectByTech: {
+            type: ProjectType,
+            args: {
+                title: {type: GraphQLString}
+            },
+            resolve(parentValue, args){
+                return axios.get('http://localhost:3000/projects/' + args.title)
                     .then(res => res.data)
             }
         }
